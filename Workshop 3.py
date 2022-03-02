53e9ba2764673f9f79fc4f04beba8d91c2bfa2b0
jasonowen875 <jasonowen875@gmail.com>
2022-03-02 20:16:53 +1100

    Create a new Workshop 3 File

---
+++ Workshop 3.py
@@ -0,0 +1,11 @@
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    name = "Jason Owen"
+    print("Hello" + name)
+    print_hi('PyCharm')
